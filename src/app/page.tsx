b6a532c1077af0aed758033e95cc3bd79b9015a8
ipko1996 <ipko1996@gmail.com>
2024-10-18 22:52:07 +0200

    add navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,52 @@
+"use client";
+
 import Link from "next/link";
-
+import { useState } from "react";
+import {
+  Autocomplete,
+  AutocompleteProps,
+  Avatar,
+  Group,
+  Text,
+} from "@mantine/core";
 import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
+import { api } from "~/trpc/react";
 
-export default async function Home() {
+export default function Home() {
+  const [value, setValue] = useState("");
+  const { data, error } = api.search.keywordQuery.useQuery({
+    keyword: "tesco",
+  });
+  console.log(data);
+
+  const renderAutocompleteOption: AutocompleteProps["renderOption"] = ({
+    option,
+  }) => (
+    <Group gap="sm">
+      <div>
+        <Link href={`/stock/${option.value.split(" ").at(-1)}`}>
+          <Text size="sm">{option.value}</Text>
+        </Link>
+      </div>
+    </Group>
+  );
+
   return (
-    <main className="">
-      <div>Search for a Stock:</div>
+    <main className="flex h-screen items-center justify-center">
+      <div className="w-1/2">
+        <Autocomplete
+          label="Search for a stock ..."
+          renderOption={renderAutocompleteOption}
+          error={error ? "Something went wrong" : ""}
+          data={
+            data?.bestMatches.map((match) => `${match.name} ${match.symbol}`) ??
+            []
+          }
+          value={value}
+          onChange={setValue}
+        />
+      </div>
     </main>
   );
 }
